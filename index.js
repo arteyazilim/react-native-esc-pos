8e24ec3c872577412dfe2d0c87de4af8c2eb2705
Gokhan Bora <gokhanweb@gmail.com>
2024-11-23 16:07:20 +0300

    index.js

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 import { DeviceEventEmitter, NativeModules } from 'react-native';
 
-const { LayoutBuilder } = NativeModules;
+const { EscPos, LayoutBuilder } = NativeModules;
 
-const EscPos = {
-  ...NativeModules.EscPos,
+const EscPosBlueTooth = {
   addListener(eventName, cb) {
     switch (eventName) {
       case 'bluetoothStateChanged':
-        EscPos.initBluetoothConnectionListener();
+        NativeModules.EscPos.initBluetoothConnectionListener();
         DeviceEventEmitter.addListener('bluetoothStateChanged', cb);
         break;
 
@@ -27,7 +26,9 @@ const EscPos = {
       return NativeModules.EscPos.connectNetworkPrinter(address, port);
     }
   },
+  disconnect() {
+    return NativeModules.EscPos.disconnect();
+  },
 };
 
-export { EscPos, LayoutBuilder };
-export default EscPos;
+export { EscPos, LayoutBuilder, EscPosBlueTooth };
